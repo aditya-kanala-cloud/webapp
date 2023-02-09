b4277ee1bd579dbe7cebea715e8023ea11800215
aditya-kanala <kanala.a@northeastern.edu>
2023-02-09 03:59:12 -0500

    Updated the test file

--- test.spec.js
+++ test.spec.js
@@ -13,3 +13,14 @@ describe('Authentication Tests', function() {
         })
     })
 })
+
+describe('Authentication Test for Post', function() {
+    describe('Successes', function() {
+        it('Return the product for post if auth does not exist', function(done) {
+            request(server).post('/v1/product/').send({}).end(function(err, res) {
+                expect(res.statusCode).to.be.equal(401);                
+                done();
+            });
+        });
+    });
+})
