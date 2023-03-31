71f3e90da9d035a9983175e578e17482b6f9123b
aditya-kanala <kanala.a@northeastern.edu>
2023-03-31 03:36:10 -0400

    Updated mocha version

--- packer/user_data.sh
+++ packer/user_data.sh
@@ -6,11 +6,6 @@ sudo yum update
 
 sudo yum upgrade
 
-sudo amazon-linux-extras install epel -y
-
-sudo yum install curl -y
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 source ~/.bashrc
 
@@ -28,7 +23,10 @@ npm install -g npm@9.5.1
 unzip webapp.zip -d webapp-main
 
 rm webapp.zip
+sudo yum install amazon-cloudwatch-agent -y
+sudo cp -R "/home/ec2-user/webapp-main/cloud-watch-agent.json" "/opt/aws/amazon-cloudwatch-agent/etc/"
 cd webapp-main
+touch combined.log
 npm i --save
 touch webapp.service
 sudo cat >> webapp.service <<'EOF'
@@ -37,7 +35,7 @@ Description=webapp
 After=multi-user.target
 [Service]
 EnvironmentFile=/home/ec2-user/webapp-main/.env
-ExecStart=/home/ec2-user/.nvm/versions/node/v16.19.1/bin/node /home/ec2-user/webapp-main/server.js
+ExecStart=/usr/bin/node /home/ec2-user/webapp-main/server.js
 Restart=always
 RestartSec=10
 StandardOutput=syslog
